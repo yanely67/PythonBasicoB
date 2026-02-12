db81a31180f20ab5b881e543b17bd1d292091f4c
unknown <yeimyvazquez67@gmail.com>
2026-02-12 07:17:44 -0800

    Documente los numeros y operadores matematicos que se usan en Python

---
+++ session_01.py
@@ -0,0 +1,35 @@
+# Numerio y flotantes
+print(int(7))
+print(float(7.7))
+print(type(7))
+print(type(7.7))
+print(int(1+2))
+print(int(10*2))
+print("===== Operadores Matematicos =====")
+# +
+# -
+# *
+# /
+# **
+# % Modulo
+print(int(2**3))
+print(int(4**8))
+print(float(10%3))
+print(float(25%4))
+print(float(16%2))
+print(float(10 / 3))
+
+ventas = 199999
+print("Nuestras ventas fueron: ", ventas)
+
+is_active = True
+print(bool(is_active))
+
+game_over = False
+print(game_over)
+
+edad = 18
+if (edad >= 18):
+    print("Si puedo entrar a el Bar!")
+else:
+    print("No puedo entrar a el Bar!")
